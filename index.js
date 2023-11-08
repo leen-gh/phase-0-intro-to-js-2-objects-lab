9351da3f55dc772253dfebc7ed6240e734d1c84c
leen-gh <leen.ghaleb.d@gmail.com>
2023-11-08 04:57:52 +0300

    assigment is done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee ={
+    name: "nobody",
+    streetAddress: "somewhere"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = { ...employee }
+    updatedEmployee[key] = value
+    //Object.assign(employee, { key: value})
+    
+    return updatedEmployee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    
+    employee[key] = value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const updatedEmployee = { ...employee }
+    delete updatedEmployee[key]
+    //Object.assign(employee, { key: value})
+    return updatedEmployee
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    //const updatedEmployee = { ...employee }
+    delete employee[key]
+    //Object.assign(employee, { key: value})
+    return employee
+}
